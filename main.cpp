7c00e2c63f707d2ec97bfda9c9f642afccdd6408
wyidongh <wyidongh@163.com>
2026-07-04 04:32:39 +0000

    Format source code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     std::cout << "Hello CI/CD World" << std::endl;
     return 0;
 }
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     std::cout << "TEST OK" << std::endl;
     return 0;
 }
